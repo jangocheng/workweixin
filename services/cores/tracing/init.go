846aa530c1753a909bbda572f4ea85919ffda903
vnotes <vnotes@protonmail.com>
2020-01-17 18:19:04 +0800

    done-tracing

--- services/cores/tracing/init.go
+++ services/cores/tracing/init.go
@@ -3,23 +3,32 @@ package tracing
 import (
 	"io"
 	"log"
+	"os"
 
 	"github.com/opentracing/opentracing-go"
 	"github.com/uber/jaeger-client-go"
 	"github.com/uber/jaeger-client-go/config"
-
 )
 
 func Init(service string) (opentracing.Tracer, io.Closer) {
+	agentHost := os.Getenv("JAEGER_AGENT_HOST")
+	agentPort := os.Getenv("JAEGER_AGENT_PORT")
+	if agentHost == "" {
+		log.Fatal("jaeger agent host should not be null")
+	}
+	if agentPort == "" {
+		agentPort = "6831"
+	}
 	cfg := &config.Configuration{
 		Sampler: &config.SamplerConfig{
-			Type:  "const",
+			Type:  jaeger.SamplerTypeConst,
 			Param: 1,
 		},
 		Reporter: &config.ReporterConfig{
-			LogSpans: true,
+			LogSpans:           true,
+			LocalAgentHostPort: agentHost + ":" + agentPort,
 		},
-		ServiceName:service,
+		ServiceName: service,
 	}
 	tracer, closer, err := cfg.NewTracer(config.Logger(jaeger.StdLogger))
 	if err != nil {
